351afc09b9a4dddd0ad0e7af7de28956cb03f454
whoombat <whoombat@gmail.com>
2024-12-12 01:41:55 +0000

    Day 1 cleanup and add some comments

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -3,32 +3,38 @@
 
 import pandas as pd
 
+# header=None is needed because the data doesn't have headers
+# without this, the first row is skipped
 df = pd.read_csv('input.txt', sep=r'\s+', header=None)
 
-
-sum = 0
-similarity_score = 0
-
-first_array = []
-second_array = []
+first_list = []
+second_list = []
 
 for i, row in enumerate(df.itertuples(), 1):
-    #print("i:" + str(i) + " row:" + str(row))
-    first_array.append(row[1])
-    second_array.append(row[2])
+    # print("i:" + str(i) + " row:" + str(row))
+    first_list.append(row[1])
+    second_list.append(row[2])
 
-first_array.sort()
-second_array.sort()
+# only part 1 needs a sort, but doesn't hurt part 2
+first_list.sort()
+second_list.sort()
 
-for (first, second) in zip(first_array, second_array):
+# part 1
+sum = 0
+for (first, second) in zip(first_list, second_list):
     abs_result = abs(first - second)
-    #print("first:" + str(first) + " second:" + str(second) + " abs:" + str(abs_result))
+    # print("first:" + str(first) + " second:" + str(second) + " abs:" + str(abs_result))
     sum += abs_result
 
-for first in first_array:
-    for second in second_array:
+
+# part 2
+similarity_score = 0
+for first in first_list:
+    for second in second_list:
         if first == second:
+            # instead of counting the instances and multiplying, just add each time
             similarity_score += first
+
 
 print("Part 1 sum:" + str(sum))
 print("Part 2 similarity score:" + str(similarity_score))
